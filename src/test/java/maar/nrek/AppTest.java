acd542639ec136e053cf7018918405747655abf6
Nesrine REKAI <nesrine.rekai@utoulouse.fr>
2026-01-15 21:46:29 +0100

    Add: tests pour App.java - couverture 100%

--- src/test/java/maar/nrek/AppTest.java
+++ src/test/java/maar/nrek/AppTest.java
@@ -1,19 +1,39 @@
 package maar.nrek;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testMainMethodPrintsHelloWorld() {
+        // Given - Capturer la sortie console
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        // When - Exécuter la méthode main
+        App.main(new String[]{});
+
+        // Then - Vérifier que "Hello World!" est affiché
+        String output = outContent.toString().trim();
+        assertEquals("Hello World!", output);
+
+        // Restaurer System.out
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testAppCanBeInstantiated() {
+        // Given/When
+        App app = new App();
+
+        // Then
+        assertNotNull(app);
     }
 }
